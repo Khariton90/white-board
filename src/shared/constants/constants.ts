e3ccf4d1775c32248b4fc8e1b1f1b532b7f71e2d
khariton90 <chinatruckspb@gmail.com>
2024-08-26 23:24:19 +0300

    remove magic numbers

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -2,7 +2,5 @@ export const BoardSize = {
 	width: 1200,
 	height: 600,
 }
-
 export const FPS = 60
-
 export const DEFAULT_COLOR = '#ff0000'
--- src/shared/lib/testForAABB.ts
+++ src/shared/lib/testForAABB.ts
@@ -1,14 +1,17 @@
-import { Player } from '../../entities/player'
+import { PlayerEntity } from '../../entities'
 import { Bullet } from './types/bullet.interface'
+import { Radius } from './types/types'
 
-export const testForAABB = (object1: Player, object2: Bullet) => {
+const inner = 10
+
+export const testForAABB = (object1: PlayerEntity, object2: Bullet) => {
 	const bounds1 = object1.getBounds()
 	const bounds2 = object2.getBounds()
 
 	return (
 		bounds1.x < bounds2.x + bounds2.width &&
-		bounds1.x + bounds1.width > bounds2.x + 50 &&
-		bounds1.y < bounds2.y + bounds2.height + 40 &&
-		bounds1.y + bounds1.height - 40 > bounds2.y
+		bounds1.x + bounds1.width > bounds2.x + Radius.Player &&
+		bounds1.y < bounds2.y + bounds2.height + Radius.Player - inner &&
+		bounds1.y + bounds1.height - Radius.Player - inner > bounds2.y
 	)
 }
